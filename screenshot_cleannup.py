27c99aa09e3452b88ac83409858515fdf0585645
eda3 <gerorin1010@gmail.com>
2020-01-03 13:18:05 +0900

    first commit

---
+++ screenshot_cleannup.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+import os
+import shutil
+from pathlib import Path
+import shutil
+
+# 前準備
+# 1. スクリーンショット格納ディレクトリに当該スクリプトを配置している
+# 2. 同ディレクトリに、以下の名前のディレクトリが作成してある
+# ディレクトリ名：archives
+
+# 作業ディレクトリ
+p = Path(".")
+
+# 移動先ディレクトリ
+archives_dir = Path("./archives")
+print(archives_dir.resolve())
+
+for f in p.glob("スクリーン*png"):
+    pass
+    # ファイルの移動開始
+    src_path = f.resolve()
+    dst_path = archives_dir.resolve() / f
+    shutil.move(src_path, dst_path)
